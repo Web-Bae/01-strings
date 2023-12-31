b70ac6807623d59b00c6992c5d57dfa51dc9edba
mediocredesigns <150205551+mediocredesigns@users.noreply.github.com>
2023-12-31 05:44:04 -0600

    section 1
    completeed 1-0

--- src/01-0-basics.js
+++ src/01-0-basics.js
@@ -4,10 +4,16 @@
 
 // 1. Define three string variables firstName, middleName, and lastName.
 
+let firstName = "tim";
+let middleName = "joseph";
+let lastName = "Quealy";
+
 // 2. Declare a function named logFullName that takes no arguments.
 function logFullName() {
   // 3. Using template literals, create another variable fullName that combines all three names.
   // 4. Print the fullName to the console.
+  let fullName = `${firstName} ${middleName} ${lastName}`;
+  console.log(fullName);
 }
 
 /**
@@ -15,17 +21,26 @@ function logFullName() {
  */
 
 // 1. Declare a variable named age and assign it a number.
+let age = 40;
+let isStudent = false;
+let courses = ["math", "CS", "English"];
 // 2. Declare a variable named isStudent and assign it a boolean value.
 // 3. Declare a variable named courses and assign it an array containing three string values representing courses e.g. "Math", "Science", "History".
 
 // 4. Declare a function named logVariableTypes that takes no arguments.
 function logVariableTypes() {
   // 5. Print the type of each variable using the typeof operator.
+  console.log(typeof age);
+  console.log(typeof isStudent);
+  console.log(typeof courses);
 }
 
 /**
  * Task 3: Variables Declaration
  */
+var school = "Hogwarts";
+let subject = "Potions";
+const professor = "Snape";
 
 // 1. Using var, declare a variable named school and assign it a value of "Hogwarts".
 // 2. Using let, declare a variable named subject and assign it a value of "Potions".
@@ -37,19 +52,26 @@ function logVariableTypes() {
 
 // 1. Declare two variables x and y with values 5 and 10 respectively.
 
+let x = 5;
+let y = 10;
+let u = 15;
+
 function logAddition() {
-  // 2. Log the sum of x and y.
+  console.log(x + y);
 }
 function logSubtraction() {
   // 3. Log the x subtracted from y
+  console.log(y - x);
 }
 
 function logMultiplication() {
   // 4. Log the product of x and y.
+  console.log(x * y);
 }
 
 function logDivision() {
   // 5. Log the quotient when x is divided by y.
+  console.log(x / y);
 }
 
 /**
@@ -64,4 +86,6 @@ let result2 = (3 + 4) * 5;
 
 function logResults() {
   // 3. Log both result values in your JavaScript environment and check your answers.
+  console.log(result1);
+  console.log(result2);
 }
