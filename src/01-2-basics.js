5d33fcebcb11e891981d618707fe9b130e505074
mediocredesigns <150205551+mediocredesigns@users.noreply.github.com>
2023-12-31 11:49:10 -0600

    lesson 01 - done
    now completed module 1

--- src/01-2-basics.js
+++ src/01-2-basics.js
@@ -3,7 +3,9 @@
  */
 
 // 1. Declare a function named generateBandName that takes two arguments: clothingColor and lastFoodEaten.
+
 // 2. In the function, declare a variable named bandName and assign it to an initial value of an empty string.
+
 // 3. Using string concatenation, assign bandName to the string "The " plus the clothingColor and lastFoodEaten variables.
 // 4. Ensure the first letter of each word is capitalized.
 // 5. Return the bandName variable.
@@ -11,6 +13,21 @@
 function generateBandName(clothingColor, lastFoodEaten) {
   // Your code here
   // Initialize bandName to an empty string
+
+  let bandName = "";
+
   // Function to capitalize the first letter of each word
-  // Construct the band name
+
+  function capitalizeWords(word) {
+    if (typeof word === "number") return word.toString();
+    if (typeof word !== "string" || word === null) return "";
+    let newWord = `${word.charAt(0).toUpperCase()}${word
+      .slice(1)
+      .toLowerCase()}`;
+    return newWord;
+  }
+
+  return `The ${capitalizeWords(clothingColor)} ${capitalizeWords(
+    lastFoodEaten
+  )}`;
 }
